0205cff9f5eba0c54ba1ae70578bd4869f42a492
Ukjan-Baek <peobin297@gmail.com>
2020-10-14 12:43:53 +0900

    proxy setup

--- servers/server.js
+++ servers/server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const app = express();
-const cors = require('cors');
+// const cors = require('cors');
 const bodyParser = require('body-parser');
 const port =process.env.PORT || 3001;
 
-app.use(cors());
+// app.use(cors());
 
 app.use(bodyParser.json());
-app.use('/api', (req, res)=> res.json({username:'욱잔'}));
+app.use('/api', route);
 
 app.listen(port, () => {
     console.log(`express is running on ${port}`);
