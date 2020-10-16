97c7debc0644910a2d4a42c1aa856cd12bcd0744
Ukjan-Baek <peobin297@gmail.com>
2020-10-16 15:06:38 +0900

    상단 카테고리 생성

--- src/App.js
+++ src/App.js
@@ -1,32 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
+import MyComponent from './components/MyComponent';
+import Counter from './components/Counter'
 import './App.css';
+import Categories from './components/Categories';
 
 class App extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            username:null
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+        username:null
+    };
+  }
 
-    componentDidMount() {
-      fetch('api')
-        .then(res=>res.json())
-        .then(data=>this.setState({username: data.username}))
-    }
+  componentDidMount() {
+    fetch('api')
+      .then(res=>res.json())
+      .then(data=>this.setState({username: data.username}))
+  }
 
   render() {
     const {username} = this.state;
     return (
         <div className="App">
+            <Categories />
           <header className="App-header">
             {username ? `Hello ${username}` : 'Hello World'}
+            <MyComponent>백</MyComponent>
+            <Counter />
           </header>
         </div>
     );
-    ;
   }
 }
 
